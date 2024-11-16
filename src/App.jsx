6f0188f26813dac382a194607f83a3b175eb94b8
Mujahid <eng.mdmuzahidulislam@gmail.com>
2024-11-16 11:16:34 +0600

    navbar, dynamic component and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Home/Shared/Footer";
+import Navbar from "./components/Home/Shared/Navbar";
+
 const App = () => {
   return <>
-    <h2>Welcome to Gadget shop</h2>
+    <div className="mb-5">
+      <Navbar />
+    </div>
+    <div className="container mx-auto">
+      <Outlet />
+    </div>
+    <div className="mt-20">
+      <Footer />
+    </div>
   </>
 }
 export default App;
