7f7a9a89c4d028ab8cd5b6a10e8d2760b272d22e
Mujahid <eng.mdmuzahidulislam@gmail.com>
2024-11-16 11:19:49 +0600

    featured products, user reviews and FAQ section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,7 @@
 import Banner from '../components/Home/Banner';
+import FAQ from '../components/Home/FAQ';
+import FeaturedProducts from '../components/Home/FeaturedProducts';
+import UserReviews from '../components/Home/UserReviews';
 
 const Home = () => {
     return (
@@ -6,6 +9,21 @@ const Home = () => {
 
             <section>
                 <Banner />
+            </section>
+
+            <section>
+                <h2 className='text-2xl mb-6 font-bold text-center'>Featured Products</h2>
+                <FeaturedProducts />
+            </section>
+
+            <section>
+                <h2 className='text-2xl mb-6 font-bold text-center'>Customer Review's</h2>
+                <UserReviews />
+            </section>
+
+            <section>
+                <h2 className='text-2xl mb-6 font-bold text-center'>Frequently Asked Questions</h2>
+                <FAQ />
             </section>
         </div>
     );
